bec88818ecd65c8069839e72e293f48744adf046
marekboro <marek.borowicz83@gmail.com>
2022-03-29 16:19:36 +0100

    added beeps

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+Console.Beep();
+Console.Beep();
+System.Threading.Thread.Sleep(1000);
+Console.Beep();
+Console.Beep();
+System.Threading.Thread.Sleep(1000);
+Console.Beep();
+Console.Beep();
